f475b0cdda61c0ec8af5917e3301fd52a827f201
zifenw <zifenw@uw.edu>
2024-10-29 21:04:32 -0700

    my home page!!!

---
+++ script.js
@@ -0,0 +1,17 @@
+let navbarItem=document.querySelectorAll('.item')
+for (let i = 0; i< navbarItem.length; i++){
+    navbarItem[i].addEventListener('click', function (){
+        for(let i = 0; i< navbarItem.length; i++){
+            navbarItem[i].classList.remove('active')
+        }
+        this.classList.add('active')
+    })
+}
+
+var typed = new Typed(".multiple-text", {
+    strings: ['UW Allen School Undergraduate', "Full-Stuck Developer","***********"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
